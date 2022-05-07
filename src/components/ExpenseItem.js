9d372a4e7ce0293c77dc8d78fa7d70bddde6025b
luciabrumnich <luciabrumnichhr@gmail.com>
2022-05-06 21:12:17 -0300

    added component ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
-  return <div>ExpenseItem</div>;
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className='expense-item__description'>
+          <h2>Description</h2>
+          <div className='expense-item__price'>Price</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
